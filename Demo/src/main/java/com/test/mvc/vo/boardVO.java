8aefb7988884e871b5753361f40d1ed9962f5f9a
TY <TY@DESKTOP-EUUPST5>
2020-12-02 16:13:49 +0900

    v1

---
+++ Demo/src/main/java/com/test/mvc/vo/boardVO.java
@@ -0,0 +1,146 @@
+package com.test.mvc.vo;
+
+import java.sql.Timestamp;
+
+public class boardVO {
+	private String title;
+	private String content;
+	private int seq;
+	private String writer;
+	private String password;
+	private Timestamp regDate;
+	private int cnt;
+
+	@Override
+	public String toString() {
+		return "boardVO [title=" + title + ", content=" + content + ", seq=" + seq + ", writer=" + writer
+				+ ", password=" + password + ", regDate=" + regDate + ", cnt=" + cnt + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + cnt;
+		result = prime * result + ((content == null) ? 0 : content.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((regDate == null) ? 0 : regDate.hashCode());
+		result = prime * result + seq;
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		result = prime * result + ((writer == null) ? 0 : writer.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		boardVO other = (boardVO) obj;
+		if (cnt != other.cnt)
+			return false;
+		if (content == null) {
+			if (other.content != null)
+				return false;
+		} else if (!content.equals(other.content))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (regDate == null) {
+			if (other.regDate != null)
+				return false;
+		} else if (!regDate.equals(other.regDate))
+			return false;
+		if (seq != other.seq)
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		if (writer == null) {
+			if (other.writer != null)
+				return false;
+		} else if (!writer.equals(other.writer))
+			return false;
+		return true;
+	}
+
+	public boardVO(String title, String content, int seq, String writer, String password, Timestamp regDate, int cnt) {
+		super();
+		this.title = title;
+		this.content = content;
+		this.seq = seq;
+		this.writer = writer;
+		this.password = password;
+		this.regDate = regDate;
+		this.cnt = cnt;
+	}
+
+	public boardVO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public int getSeq() {
+		return seq;
+	}
+
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Timestamp getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(Timestamp regDate) {
+		this.regDate = regDate;
+	}
+
+	public int getCnt() {
+		return cnt;
+	}
+
+	public void setCnt(int cnt) {
+		this.cnt = cnt;
+	}
+}
